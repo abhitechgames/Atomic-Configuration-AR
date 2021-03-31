f2d4ac2425a574a187dbee6633aacb0fa7d17ff7
Abhishek Singh <abhisheksingh360degree@gmail.com>
2021-03-31 20:04:06 +0530

    uploading files

---
+++ Electronic Configuration AR/Assets/Scripts/RotOfSubAtomicParticles.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotOfSubAtomicParticles : MonoBehaviour
+{
+    public Vector3 rotSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotSpeed * Time.deltaTime);
+    }
+}
